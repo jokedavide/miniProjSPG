773136abd6c42033032b9967a805f5bffa0e56f4
Izzah <izzahfaudzee@gmail.com>
2025-11-10 14:23:44 +0800

    Add files via upload

---
+++ app/Models/Customer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    // Add this property to allow mass assignment
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+    ];
+}
